3deafed651ede49ec27196e0da90bd6f0258125c
andrisilaban <andriantosilaban@gmail.com>
2023-10-10 11:24:13 +0700

    Add Seed Table Users

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use DB;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,6 +14,31 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert(
+            [
+                'name' => 'Admin',
+                'username' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'admin',
+                'status' => 'active',
+            ],
+            [
+                'name' => 'Agent',
+                'username' => 'agent',
+                'email' => 'agent@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'agent',
+                'status' => 'active',
+            ],
+            [
+                'name' => 'User',
+                'username' => 'user',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'user',
+                'status' => 'active',
+            ],
+    );
     }
 }
